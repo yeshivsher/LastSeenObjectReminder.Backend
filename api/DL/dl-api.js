c16a176c100deb426e4fe49c4c8744d4af673019
Yeshivsher <rafisem7@gmail.com>
2021-03-15 23:50:28 +0200

    fix

--- api/DL/dl-api.js
+++ api/DL/dl-api.js
@@ -4,13 +4,7 @@ const getIsObjectInTheImageOrNot = (clientId) => {
 }
 
 const InitCurrentImageSaver = async () => {
-    await extractFrames({
-        input: '../../video/testVideo.mp4',
-        output: './currentImage-%i.jpg',
-        offsets: [
-            1000
-        ]
-    })
+
 }
 
 
