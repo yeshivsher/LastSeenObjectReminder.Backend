8a0b0f34350f0fda64e00c5aa758c39004601446
Yeshivsher <rafisem7@gmail.com>
2021-05-20 17:35:25 +0300

    2 cameras

--- api/service/camera-service.js
+++ api/service/camera-service.js
@@ -1,4 +1,5 @@
 const VideoCapture = require('camera-capture')
+const fetch = require("node-fetch");
 
 const fs = require('fs');
 const SECONDS_CUPTURE = 0.1
@@ -14,7 +15,15 @@ function base64_encode(file) {
   return new Buffer(bitmap).toString('base64');
 }
 
-const getNoImage = async () => {
+const checkNoContentResponse = async response => {
+  if (response.status === NO_CONTENT_STATUS_CODE) {
+    return []
+  } else {
+    return response.json()
+  }
+}
+
+const getNoImage = async (url) => {
   var bitmap = base64_encode(__dirname + url)
 
   console.log("🚀 ~ file: camera-service.js ~ line 13 ~ getNoImage ~ bitmap", bitmap)
@@ -27,10 +36,47 @@ const InitCurrentImageSaver = async () => {
 
   setInterval(async () => {
     let f = await c.readFrame()
-    fs.writeFileSync('./image/currentImage/tmp.png', f.data)
+    let result = {};
+    let query = "DLURL/";
 
-    console.log('Capture...')
+    fs.writeFileSync('./image/currentImage/tmpCamera1.png', f.data)
+    console.log('Camera 1, capture... ')
+
+    fetch(query)
+      .then(checkNoContentResponse)
+      .then(data => {
+        result = data;
+        resolve(result);
+      })
+      .catch(e => {
+        result = e
+        reject(result);
+      });
+    console.log('Send camera 1 pic to DL service...')
+
   }, SECONDS_CUPTURE * 1000)
+
+  // Add camera 2
+  // setInterval(async () => {
+  //   let f = await c.readFrame()
+  //   let result = {};
+  //   let query = "DLURL/";
+
+  //   fs.writeFileSync('./image/currentImage/tmpCamera2.png', f.data)
+  //   console.log('Camera 2, capture... ')
+
+  //   fetch(query)
+  //     .then(checkNoContentResponse)
+  //     .then(data => {
+  //       result = data;
+  //       resolve(result);
+  //     })
+  //     .catch(e => {
+  //       result = e
+  //       reject(result);
+  //     });
+  //   console.log('Send camera 2 pic to DL service...')
+  // }, SECONDS_CUPTURE * 1000)
 
   console.log('\nSave image each ' + SECONDS_CUPTURE + ' seconds\nOutput: image/currentImage/tmp.png\n\n')
 }
--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const serverRestApi = require('./api/web-api/server-rest-api')
 const cameraService = require('./api/service/camera-service')
 
 // Init servers.
-// cameraService.InitCurrentImageSaver()
+
+/// uncomment only when url is correct
+//cameraService.InitCurrentImageSaver()
 serverRestApi.initRestApiServer()
